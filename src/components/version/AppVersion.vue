d2457aa29a22f874c7e168459fb2841c61a12a46
TATA <hostit313@gmail.com>
2024-11-28 02:11:42 +0700

    🚀feat(AppVersion) add environment variable for app version; update version display in AppVersion component and increment package version to 0.0.1

--- src/components/version/AppVersion.vue
+++ src/components/version/AppVersion.vue
@@ -1,5 +1,12 @@
 <template>
-  <p>Version 1.0.0</p>
+  <p>Version {{ appVersion }}</p>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+
+// Access the version from the environment
+const appVersion = ref(import.meta.env.VITE_APP_VERSION);
+
+console.log("App Version:", appVersion.value); // Debugging output
+</script>
