9e07a107df7cc27d4f125cab44470428f97e1340
TATATATA <hostit313@gmail.com>
2025-08-06 22:36:59 +0700

    🚀feat(Firebase) add Firebase configuration to .env and .env.example; update init.js to use environment variables for Firebase settings

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -10,13 +10,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCtoZwrM6MHC6SmMC6slv6pFJDcsIVEgTg",
-  authDomain: "prompos-f3b52.firebaseapp.com",
-  projectId: "prompos-f3b52",
-  storageBucket: "prompos-f3b52.firebasestorage.app",
-  messagingSenderId: "119446721235",
-  appId: "1:119446721235:web:95564d66cf697e09a3ba26",
-  measurementId: "G-04Z0HJVKF5",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
